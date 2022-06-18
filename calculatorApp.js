f924158c85fbbc4274a95193fdd7da031235bb68
Aste <aste@tuta.io>
2022-06-19 00:56:55 +0200

    Add styling, change display to div

--- calculatorApp.js
+++ calculatorApp.js
@@ -1,26 +1,51 @@
+// Odin exercise: Build a calculator with basic functionality, without using the eval() function
+
 // Declare variables from document
 const allKeys = document.querySelectorAll('#allKeys button')
-let displayAttribute = document.getElementsByClassName("display")[0]
-console.log(displayAttribute)
+let displayVal = document.getElementById("display").value
+let calVal = 0;
 
-const add = (num1, num2) => (num1 + num2)
-const subtract = (num1, num2) => (num1 - num2)
-const multiply = (num1, num2) => (num1 * num2)
-const divide = (num1, num2) => (num1 / num2)
+
+// Declare operators and operate functions
+const add = (num1, num2) => console.log(num1 + num2)
+const subtract = (num1, num2) => console.log(num1 - num2)
+const multiply = (num1, num2) => console.log(num1 * num2)
+const divide = (num1, num2) => console.log(num1 / num2)
 const operate = (operator, num1, num2) => operator(num1, num2)
 
 // Loop through all keys
 for (var i = 0; i < allKeys.length; i++) {
     //add onclick event to each key
     allKeys[i].onclick = function (e) {
-        // assign buttonValue from HTML value
-        let buttonValue = this.innerHTML
+        // assign buttonVal from HTML value
+        let buttonVal = this.innerHTML
+        let buttonClass = this.className
 
         // add number value to numerical keys
-        if (!isNaN(buttonValue)) {
-            displayAttribute.setAttribute("value", displayAttribute.getAttribute("value") + buttonValue)
+        if (!isNaN(buttonVal)) {
+            // display.setAttribute("value", display.getAttribute("value") + buttonVal)
+            displayVal = displayVal + buttonVal
+            console.log(displayVal);
         }
 
+        // implement operator functions
+        if (buttonClass == "operator") {
+            if (this.innerHTML == "+") { add(1, 2) }
+            if (this.innerHTML == "-") { subtract(1, 2) }
+            if (this.innerHTML == "x") { multiply(1, 2) }
+            if (this.innerHTML == "/") { divide(1, 2) }
+            if (this.innerHTML == "AC") {
+                console.log(displayVal.value)
+                displayVal.value = "1"
+                console.log(displayVal)
+            }
+            if (this.innerHTML == "±") { displayVal.value *= -1 }
+            if (this.innerHTML == "=") { console.log(displayVal.value) }
+            if (this.innerHTML == "%") {
+                // displayVal.value /= 100
+                displayVal.setAttribute("value", displayVal.getAttribute("value") + "1")
+                console.log(displayVal.value)
+            }
+        }
     }
-
 }
